757f5636da4232ee5a9f9e807a8ee095f6333c43
paulgithub.8 <Ot_moroz_ochek.8@mail.ru>
2025-02-04 14:35:03 +0500

    калькулятор

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,36 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
 
     public static void main(String[] args) {
         //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
+
         a = inputDouble();
         b = inputDouble();
-
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+        operation();
     }
 
-    private static void divide() {
+
+    public static void operation() {
+        String op ;
+        System.out.println("Введите действие");
+        op = new Scanner(System.in).nextLine();
+        if(op.equals ("+")){
+            sum();
+        }
+        if(op.equals("-")){
+            diff();
+        }
+        if(op.equals("*")){
+            multiply();
+        }
+        if(op.equals("/")){
+            divide();
+        }
+    }
+
+    public static void divide() {
+        System.out.println(a / b);
         //todo дописать логику и печать результата
     }
 
-    private static void diff() {
+    public static void diff() {
+        System.out.println(a - b);
         //todo дописать логику и печать результата
     }
 
-    private static void multiply() {
+    public static void multiply() {
+        System.out.println(a * b);
         //todo дописать логику и печать результата
     }
 
-    private static void sum() {
+    public static void sum() {
+        System.out.println(a + b);
         //todo дописать логику и печать результата
     }
 
-    private static double inputDouble() {
+    public static double inputDouble() {
+        double x ;
+        System.out.println("Введите число ");
+        x = new Scanner(System.in).nextDouble();
         //todo дописать логику
-        return 0;
+        return x ;
     }
 }
